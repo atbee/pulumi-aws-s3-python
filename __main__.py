44a6b71c8b240a839064905bb02d513e0f69b8fe
atbee <atb@odds.team>
2021-07-20 23:08:01 +0700

    Set bucket name in config parameter :bee:

--- __main__.py
+++ __main__.py
@@ -1,10 +1,14 @@
 import pulumi
 from pulumi_aws import s3
 
+
+config = pulumi.Config()
+bucket_name = config.require("bucket-name")
+
 # Create AWS resources
 bucket = s3.Bucket(
-    "atbee-name",
-    bucket="atbee-name",
+    bucket_name,
+    bucket=bucket_name,
 )
 
 # Output
