354505bb324e79a5fd344a132eb12dc235fee4ee
atbee <atb@odds.team>
2021-07-20 23:10:39 +0700

    Enable default S3 encryption :bee:

--- __main__.py
+++ __main__.py
@@ -10,6 +10,14 @@ bucket = s3.Bucket(
     bucket_name,
     acl="private",
     bucket=bucket_name,
+    server_side_encryption_configuration=s3.BucketServerSideEncryptionConfigurationArgs(
+        rule=s3.BucketServerSideEncryptionConfigurationRuleArgs(
+            bucket_key_enabled=False,
+            apply_server_side_encryption_by_default=s3.BucketServerSideEncryptionConfigurationRuleApplyServerSideEncryptionByDefaultArgs(
+                sse_algorithm="AES256",
+            ),
+        ),
+    ),
 )
 
 # Output
